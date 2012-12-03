939ba02ee517388351323d98f7bf279cb8ebd401
freestrings <mark.han@nexr.com>
2012-12-03 16:06:15 +0900

    Ready for codeanal

---
+++ codeanal/main.js
@@ -0,0 +1,14 @@
+define(function(require, exports, module) {
+
+require("ace/lib/fixoldbrowsers");
+require("ace/config").init();
+
+var Editor = require("ace/editor").Editor;
+var Renderer = require("ace/virtual_renderer").VirtualRenderer;
+
+var editor = new Editor(new Renderer(document.getElementById("editor"), require("ace/theme/textmate")));
+editor.resize();
+editor.setTheme("ace/theme/twilight");
+editor.session.setMode("ace/mode/javascript");
+
+});
